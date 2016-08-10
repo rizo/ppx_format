79daf1532e058c7e0ace05098e1117904ad09d19
Rizo Isrof <rizo.isrof@gmail.com>
2016-08-10 10:41:20 +0100

    Update test_ppx_format.ml

--- tests/test_ppx_format.ml
+++ tests/test_ppx_format.ml
@@ -1,21 +1,19 @@
 
-let print = print_endline
-
 let () =
   let name = Unix.getenv "USER" in
   let time = Unix.time () in
   let cdir = Unix.getenv "PWD" in
-  print ("Hello %s! Current UNIX time is %f. Your current directory is %s" % (name, time, cdir))
+  print_endline ("Hello %s! Current UNIX time is %f. Your current directory is %s" % (name, time, cdir))
 
 let () =
   let name = Unix.getenv "USER" in
-  print ("Hello, %s!" % name)
+  print_endline ("Hello, %s!" % name)
 
 let () =
   let name = Unix.getenv "USER" in
-  print ((%) "Hello, %s!" name)
+  print_endline ((%) "Hello, %s!" name)
 
 let () =
   let formatter = (%) "Hello, %s!" in
-  print (formatter "world")
+  print_endline (formatter "world")
 
